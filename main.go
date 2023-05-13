8f4c5e6b2c8e116e1255357faa8124a7d11d8183
Vitalii Chura <vcvitaly@yahoo.com>
2023-05-13 09:11:37 +0300

    add test step

--- main.go
+++ main.go
@@ -22,10 +22,13 @@ func run(proj string, out io.Writer) error {
 		return fmt.Errorf("The project directory is required: %w", ErrValidation)
 	}
 
-	pipeline := make([]step, 1)
+	pipeline := make([]step, 2)
 
 	pipeline[0] = newStep(
 		"go build", "go", "Go Build: SUCCESS", proj, []string{"build", ".", "errors"},
+	)
+	pipeline[1] = newStep(
+		"go test", "go", "Go Test: SUCCESS", proj, []string{"test", "-v"},
 	)
 
 	for _, s := range pipeline {
